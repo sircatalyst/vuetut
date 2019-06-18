847a2d00f33b9b2babcf082f1208a54af1ca9543
sircatalyst <temibami@gmail.com>
2019-06-18 10:20:32 +0100

    intro to components

--- app.js
+++ app.js
@@ -117,3 +117,35 @@ var two = new Vue({
 });
 
 two.title = "Changed from outside";
+
+Vue.component('greeting', {
+    template: '<p>Hey there, I am {{name}} <button v-on:click="changeName">Change name</button></p>',
+    data: function() {
+        return {
+            name: 'Yoshi'
+        }
+    },
+    methods: {
+        changeName: function() {
+            this.name = 'Mario';
+        }
+    }
+});
+
+new Vue ({
+    el: "#vue-app-three",
+   
+});
+
+new Vue ({
+    el: "#vue-app-four",
+    data: {
+
+    },
+    methods:{
+
+    },
+    computed:{
+
+    }
+});
