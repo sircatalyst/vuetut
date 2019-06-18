23ec509d91961245714298b337e953e9e5c24724
sircatalyst <temibami@gmail.com>
2019-06-18 10:05:43 +0100

    multiple vue instances

--- app.js
+++ app.js
@@ -83,3 +83,37 @@ new Vue({
         }
     }
 });
+
+var one = new Vue({
+    el: "#vue-app-one",
+    data: {
+        title: 'Vue App One'
+    },
+    methods: {
+
+    },
+    computed: {
+        greet: function () {
+            return 'Hello from app one'
+        }
+    }
+});
+
+var two = new Vue({
+    el: "#vue-app-two",
+    data: {
+        title: 'Vue App Two'
+    },
+    methods: {
+        changeTitle: function() {
+            one.title = "Title changed"
+        }
+    },
+    computed: {
+        greet: function () {
+            return 'Hello from app two'
+        }
+    }
+});
+
+two.title = "Changed from outside";
